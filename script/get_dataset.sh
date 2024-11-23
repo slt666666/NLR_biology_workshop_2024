1f55e3a6bba31837993739dacccc5e01c2d1861f
Toshiyuki Sakai <toshi6661024@gmail.com>
2024-11-23 15:30:09 +0900

    Update get_dataset.sh

--- script/get_dataset.sh
+++ script/get_dataset.sh
@@ -9,7 +9,10 @@ for i in $(seq -f "%03g" 1 10);do
   wget -q https://github.com/slt666666/NLR_biology_workshop_2024/raw/master/data/${NAME}.part_${i}.fastq.gz
 done
 
-gunzip ${NAME}.part_001.fastq.gz > ${NAME}.part_001.fastq
+gunzip ${NAME}.part_001.fastq.gz
 
 cat ${NAME}.part_*.fastq.gz > ${NAME}.fastq.gz
 
+for i in $(seq -f "%03g" 1 10);do
+  rm ${NAME}.part_${i}.fastq.gz
+done
